407ae982091965c52052bbfa628a38c87eb4ce58
Joseph Wright <joe@joegle.com>
2017-05-07 15:05:18 -0400

    Add angular2 router from tutorial

--- app/app/app.module.ts
+++ app/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HeroListComponent } from './hero-list.component';
 import { AppComponent }  from './app.component';
 
+const appRoutes: Routes = [
+    { path: 'hero-list', component: HeroListComponent }
+
+];
+    
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
+    declarations: [ AppComponent, HeroListComponent  ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/app/hero-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    template: `
+    <h2>HEROES</h2>
+    <p>Get your heroes here</p>
+    <button routerLink="/sidekicks">Go to sidekicks</button>
+`
+})
+export class HeroListComponent { }
